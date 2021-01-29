53de6cfc3d7d29b9ad66bdda2ad7630ec4aaaf9d
Ben Hayes <b.t.hayes@comcast.net>
2021-01-29 22:31:06 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -12,7 +12,6 @@ class Student
   def self.create_from_collection(students_array)
     students_array.each do |student_hash|
       student = Student.new(student_hash)
-      student 
     end 
   end
 
